4024f4fa1ba5db9a8f4185e417168041790016e3
Filip Łukasik <fanixus96@gmail.com>
2018-10-26 19:45:06 +0200

    delete event fixed

--- src/Todo.js
+++ src/Todo.js
@@ -8,21 +8,14 @@ class Todo extends Component {
   };
 
   deleteEvent() {
-     var items = this.props.parent.state.todos;
-    //var idx = 
-    //var idx = items.value.indexOf(function(currentItem){
-     // return this.items.id == currentItem.id;
-   // });
-
-    //var reducedItems= this.items.splice(idx,1)
-
-    //this.props.setState({
-     // items: reducedItems
-    //});
-    
-
-    //console.log(idx)
-    console.log(items)
+    var items = this.props.parent.state.todos;
+    var idx = items.indexOf(this.props.item)
+      if (idx!=-1){
+        items.splice(idx,1)
+      }
+    this.props.parent.setState({
+      todos: items
+    });
   };
 
   render() {
